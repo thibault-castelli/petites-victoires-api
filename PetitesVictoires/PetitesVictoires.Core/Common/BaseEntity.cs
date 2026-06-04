4750997fe75fa78072d401356c01c2f5a88dee4e
codeintheshell <thibault.castelli@pm.me>
2026-06-04 16:10:14 +0200

    Improve base entity with creation, update and delete time

--- PetitesVictoires/PetitesVictoires.Core/Common/BaseEntity.cs
+++ PetitesVictoires/PetitesVictoires.Core/Common/BaseEntity.cs
@@ -2,5 +2,19 @@ namespace PetitesVictoires.Core.Common;
 
 public abstract class BaseEntity<TId>
 {
-    public required TId Id { get; set; }
+    public TId Id { get; set; } = default!;
+    public DateTime CreatedAt { get; init; }
+    public DateTime? UpdatedAt { get; private set; }
+    public DateTime? DeletedAt { get; private set; }
+
+    protected void MarkUpdated()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void MarkSoftDeleted()
+    {
+        DeletedAt = DateTime.UtcNow;
+        MarkUpdated();
+    }
 }
