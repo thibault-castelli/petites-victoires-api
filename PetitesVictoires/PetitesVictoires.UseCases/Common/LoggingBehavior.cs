04912a2c814c451a97696a227bb0bb2f55ee3d01
codeintheshell <thibault.castelli@pm.me>
2026-07-09 10:05:43 +0200

    Add custom logging behavior

---
+++ PetitesVictoires/PetitesVictoires.UseCases/Common/LoggingBehavior.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Mediator;
+using Microsoft.Extensions.Logging;
+
+namespace PetitesVictoires.UseCases.Common;
+
+public class LoggingBehavior<TMessage, TResponse>(ILogger<LoggingBehavior<TMessage, TResponse>> logger)
+    : IPipelineBehavior<TMessage, TResponse>
+    where TMessage : notnull, IMessage
+{
+    public async ValueTask<TResponse> Handle(TMessage message, MessageHandlerDelegate<TMessage, TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (logger.IsEnabled(LogLevel.Information))
+            logger.LogInformation("Handling {RequestName} with {@Request}", typeof(TMessage).Name, message);
+
+        var sw = Stopwatch.StartNew();
+
+        var response = await next(message, cancellationToken);
+
+        sw.Stop();
+
+        if (logger.IsEnabled(LogLevel.Information))
+            logger.LogInformation("Handled {RequestName} with {Response} in {ElapsedMilliseconds} ms",
+                typeof(TMessage).Name, response, sw.ElapsedMilliseconds);
+
+        return response;
+    }
+}
